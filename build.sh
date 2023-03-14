d293671ef0edb4179cf77407fee0b78be6fa2ade
LockBlock-dev <68129141+LockBlock-dev@users.noreply.github.com>
2023-03-14 22:03:35 +0100

    separate scripts build/install

--- build.sh
+++ build.sh
@@ -4,17 +4,3 @@
 cd src
 go build -o ..
 cd ..
-
-# Prompt for sudo
-sudo -v
-
-# Copy the service file
-sudo cp ./minepot.service /etc/systemd/system/minepot.service
-
-# Copy the config
-sudo cp ./config.json /etc/minepot/config.json
-
-# Reload and start the service
-sudo systemctl daemon-reload
-sudo systemctl enable minepot.service
-sudo systemctl start minepot.service
---
+++ install.sh
@@ -0,0 +1,13 @@
+# Prompt for sudo
+sudo -v
+
+# Copy the service file
+sudo cp ./minepot.service /etc/systemd/system/minepot.service
+
+# Copy the config
+sudo cp ./config.json /etc/minepot/config.json
+
+# Reload and start the service
+sudo systemctl daemon-reload
+sudo systemctl enable minepot.service
+sudo systemctl start minepot.service
